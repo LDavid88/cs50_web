bdcc8c27ef271379ca1f05971fa26e17171896e1
David Romero <pollinxld@gmail.com>
2023-10-06 20:10:00 -0600

    set urls.py and views.py newyear app

--- django/project1/newyear/views.py
+++ django/project1/newyear/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
+def newyear(request):
+    now = datetime.datetime.now()
+    return render(request, 'newyear/day.html', {
+        "newyear": now.month == 1 and now.day == 1
+    })
