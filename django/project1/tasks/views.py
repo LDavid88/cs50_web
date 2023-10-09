e1fea23d94f1aeb079f4a8680eb646408f2463a8
David Romero <pollinxld@gmail.com>
2023-10-09 15:52:15 -0600

    add urls.py and set views.py in tasks app

--- django/project1/tasks/views.py
+++ django/project1/tasks/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+items = ["apples", "carrots", "bananas"]
+
+def index(request):
+    return render(request, "tasks/index.html", {
+        "items": items
+    })
